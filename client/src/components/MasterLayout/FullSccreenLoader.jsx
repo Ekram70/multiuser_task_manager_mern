e122adf8a263c60ef367ec09f9ae5964b8084ffd
Ekram70 <ekramullah70@gmail.com>
2022-10-26 01:00:07 +0600

    frontend project structure

---
+++ client/src/components/MasterLayout/FullSccreenLoader.jsx
@@ -0,0 +1,7 @@
+import React from 'react';
+
+const FullSccreenLoader = () => {
+  return <div>FullSccreenLoader</div>;
+};
+
+export default FullSccreenLoader;
---
+++ client/src/pages/ForgetPassPage.jsx
@@ -0,0 +1,7 @@
+import React from 'react';
+
+const ForgetPassPage = () => {
+  return <div>ForgetPassPage</div>;
+};
+
+export default ForgetPassPage;
